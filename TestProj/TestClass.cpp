df1752964b6c59733517dc3665c9a648d89fa424
Gdek <alex967123@gmail.com>
2014-08-13 16:56:45 -0400

    Merge TestClass Branch

---
+++ TestProj/TestClass.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "TestClass.h"
+
+
+TestClass::TestClass()
+{
+	//This is a comment
+}
+
+
+TestClass::~TestClass()
+{
+}
--- TestProj/TestProj.cpp
+++ TestProj/TestProj.cpp
@@ -10,6 +10,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	//Delorted! had to edit this
 	//Made a change
 	//Some more changes
+	//Did some math
+	int i = 1;
+	int x = 1;
+	int y = x + i;
 	return 0;
 }
 
